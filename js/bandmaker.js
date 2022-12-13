07cba717bc2ca89d7b463d0e63c4a2761947205a
Tyler-Rodenberger <113135077+Tyler-Rodenberger@users.noreply.github.com>
2022-12-12 20:19:03 -0500

    Finished bandmaker

--- js/bandmaker.js
+++ js/bandmaker.js
@@ -18,12 +18,12 @@ var lead_synth;
 function bandmaker_start_stop () {
     button = document.getElementById("bandmakerbutton");
     button.addEventListener("click", bandmakerstart);
-    bandmakerstart();
+    if (!running) { bandmakerstart(); }
 }
 
 function bandmakerstart() {
     accompaniment_synth = new Tone.PolySynth(Tone.Synth).toDestination();
-    lead_synth = new Tone.Synth().toDestination();
+    lead_synth = new Tone.DuoSynth().toDestination();
     bass_synth = new Tone.Synth().toDestination();
     drums_synth = new Tone.MembraneSynth().toDestination();
     set_volumes();
@@ -130,7 +130,7 @@ function accompaniment_mute_unmute () {
 
 function accompanimentunmute() {
     console.log("accompaniment unmuted")
-    accompaniment_volume = -6;
+    accompaniment_volume = -16;
     button.removeEventListener("click", accompanimentunmute);
     button.addEventListener("click", accompanimentmute);
     button.value = "Mute";
@@ -162,7 +162,7 @@ function lead_synth_mute_unmute () {
 
 function lead_synthunmute() {
     console.log("lead_synth unmuted")
-    lead_synth_volume = -6;
+    lead_synth_volume = -12;
     button.removeEventListener("click", lead_synthunmute);
     button.addEventListener("click", lead_synthmute);
     button.value = "Mute";
@@ -183,156 +183,131 @@ function lead_synthmute() {
 
 async function play() {
     console.log("playing");
-    for (let i = 0; i < 100; i += 1) {
-        if (!running) {
-            break;
-        }
+    for (let i = 0; i < 1000; i += 1) {
+        if (!running) { break; }
         set_volumes();
         // beat 1
         drums_synth.triggerAttackRelease("C1", "32N");
-        bass_synth.triggerAttackRelease("C3", "8N");
-
+        bass_synth.triggerAttackRelease("D2", "16N");
+        accompaniment_synth.triggerAttackRelease(["D4", "F#4", "A4", "D5"], "8N");
+        lead_synth.triggerAttackRelease("D5", "4N");
         await new Promise(r => setTimeout(r, 125));
         // 1.25
-
         await new Promise(r => setTimeout(r, 125));
         // 1.5
-
+        bass_synth.triggerAttackRelease("A2", "8N");
         await new Promise(r => setTimeout(r, 125));
         // 1.75
         drums_synth.triggerAttackRelease("C2", "32N");
-
+        lead_synth.triggerAttackRelease("Db5", "16N");
         await new Promise(r => setTimeout(r, 125));
-
-        set_volumes();
         // 2
         drums_synth.triggerAttackRelease("C1", "32N");
-        bass_synth.triggerAttackRelease("Bb2", "8N");
-
+        bass_synth.triggerAttackRelease("C#3", "16N");
+        accompaniment_synth.triggerAttackRelease(["D4", "F#4", "A4", "D5"], "8N");
+        lead_synth.triggerAttackRelease("F#5", "4N");
         await new Promise(r => setTimeout(r, 125));
         // 2.25
-
+        bass_synth.triggerAttackRelease("D3", "8N");
         await new Promise(r => setTimeout(r, 125));
         // 2.5
         drums_synth.triggerAttackRelease("C2", "32N");
-
         await new Promise(r => setTimeout(r, 125));
         // 2.75
-
+        bass_synth.triggerAttackRelease("F#3", "8N");
+        lead_synth.triggerAttackRelease("A5", "16N");
         await new Promise(r => setTimeout(r, 125));
-
+        if (!running) { break; }
         set_volumes();
         // 3
         drums_synth.triggerAttackRelease("C1", "32N");
         bass_synth.triggerAttackRelease("Ab2", "8N");
-
+        accompaniment_synth.triggerAttackRelease(["D#4", "G4", "A#4", "D#5"], "8N");
+        lead_synth.triggerAttackRelease("A#5", "4N");
         await new Promise(r => setTimeout(r, 125));
         // 3.25
-
         await new Promise(r => setTimeout(r, 125));
         // 3.5
-
+        bass_synth.triggerAttackRelease("A#3", "16N");
         await new Promise(r => setTimeout(r, 125));
         // 3.75
         drums_synth.triggerAttackRelease("C2", "32N");
-
+        bass_synth.triggerAttackRelease("A3", "16N");
         await new Promise(r => setTimeout(r, 125));
-        
-        set_volumes();
         // 4
         drums_synth.triggerAttackRelease("C1", "32N");
-        bass_synth.triggerAttackRelease("G2", "8N");
-
+        bass_synth.triggerAttackRelease("G3", "16N");
         await new Promise(r => setTimeout(r, 125));
         // 4.25
-
+        bass_synth.triggerAttackRelease("C#3", "16N");
         await new Promise(r => setTimeout(r, 125));
         // 4.5
         drums_synth.triggerAttackRelease("C2", "32N");
         bass_synth.triggerAttackRelease("B2", "8N");
-
+        lead_synth.triggerAttackRelease("A#5", "8N");
         await new Promise(r => setTimeout(r, 125));
         // 4.75
-
+        bass_synth.triggerAttackRelease("C#3", "16N");
+        await new Promise(r => setTimeout(r, 125));
+        if (!running) { break; }
+        // 5
+        drums_synth.triggerAttackRelease("C1", "32N");
+        bass_synth.triggerAttackRelease("D3", "8N");
+        accompaniment_synth.triggerAttackRelease(["D4", "F#4", "A4", "D5"], "8N");
+        lead_synth.triggerAttackRelease("D5", "2N");
+        await new Promise(r => setTimeout(r, 125));
+        // 5.25
+        await new Promise(r => setTimeout(r, 125));
+        // 5.5
+        await new Promise(r => setTimeout(r, 125));
+        // 5.75
+        drums_synth.triggerAttackRelease("C2", "32N");
+        bass_synth.triggerAttackRelease("D#3", "16N");
+        await new Promise(r => setTimeout(r, 125));
+        // 6
+        drums_synth.triggerAttackRelease("C1", "32N");
+        bass_synth.triggerAttackRelease("A#2", "16N");
+        await new Promise(r => setTimeout(r, 125));
+        // 6.25
+        await new Promise(r => setTimeout(r, 125));
+        // 6.5
+        drums_synth.triggerAttackRelease("C2", "32N");
+        accompaniment_synth.triggerAttackRelease(["D4", "F#4", "A4", "D5"], "8N");
+        await new Promise(r => setTimeout(r, 125));
+        // 6.75
+        bass_synth.triggerAttackRelease("A2", "16N");
+        await new Promise(r => setTimeout(r, 125));
+        if (!running) { break; }
+        set_volumes();
+        // 7
+        drums_synth.triggerAttackRelease("C1", "32N");
+        bass_synth.triggerAttackRelease("G2", "16N");
+        accompaniment_synth.triggerAttackRelease(["D#4", "G4", "A#4", "D#5"], "8N");
+        lead_synth.triggerAttackRelease("Db5", "4N");
+        await new Promise(r => setTimeout(r, 125));
+        // 7.25
+        await new Promise(r => setTimeout(r, 125));
+        // 7.5
+        await new Promise(r => setTimeout(r, 125));
+        // 7.75
+        drums_synth.triggerAttackRelease("C2", "32N");
+        bass_synth.triggerAttackRelease("F#2", "16N");
+        await new Promise(r => setTimeout(r, 125));
+        // 8
+        drums_synth.triggerAttackRelease("C1", "32N");
+        bass_synth.triggerAttackRelease("D#2", "16N");
+        lead_synth.triggerAttackRelease("A#4", "8N");
+        await new Promise(r => setTimeout(r, 125));
+        // 8.25
+        await new Promise(r => setTimeout(r, 125));
+        // 8.5
+        drums_synth.triggerAttackRelease("C2", "32N");
+        bass_synth.triggerAttackRelease("A2", "8N");
+        lead_synth.triggerAttackRelease("A4", "16N");
+        await new Promise(r => setTimeout(r, 125));
+        // 8.75
+        lead_synth.triggerAttackRelease("A#5", "16N");
         await new Promise(r => setTimeout(r, 125));
     }
     console.log("done playing");
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// async function cmaccompaniment() {
-//     console.log("Insert accompaniment here");
-//     // accompaniment_synth.volume.value = accompaniment_volume;
-//     // accompaniment_synth.triggerAttack("C4");
-//     // accompaniment_synth.triggerAttack("Eb4");
-//     // accompaniment_synth.triggerAttack("G4");
-//     // accompaniment_synth.triggerAttack("Bb4");
-//     // await new Promise(r => setTimeout(r, 800));
-//     // accompaniment_synth.triggerRelease(["C4", "Eb4", "G4", "Bb4"])
-//     // await new Promise(r => setTimeout(r, 200));
-//     //
-//     // accompaniment_synth.volume.value = accompaniment_volume;
-//     // accompaniment_synth.triggerAttack("Bb2");
-//     // accompaniment_synth.triggerAttack("D3");
-//     // accompaniment_synth.triggerAttack("F3");
-//     // accompaniment_synth.triggerAttack("Ab3");
-//     // await new Promise(r => setTimeout(r, 800));
-//     // accompaniment_synth.triggerRelease(["Bb2", "D3", "F3", "Ab3"])
-//     // await new Promise(r => setTimeout(r, 200));
-//     //
-//     // accompaniment_synth.volume.value = accompaniment_volume;
-//     // accompaniment_synth.triggerAttack("Ab2");
-//     // accompaniment_synth.triggerAttack("C3");
-//     // accompaniment_synth.triggerAttack("Eb3");
-//     // accompaniment_synth.triggerAttack("Gb3");
-//     // await new Promise(r => setTimeout(r, 800));
-//     // accompaniment_synth.triggerRelease(["Ab2", "C3", "Eb3", "Gb3"])
-//     // await new Promise(r => setTimeout(r, 200));
-//     //
-//     // accompaniment_synth.volume.value = accompaniment_volume;
-//     // accompaniment_synth.triggerAttack("G2");
-//     // accompaniment_synth.triggerAttack("B2");
-//     // accompaniment_synth.triggerAttack("D3");
-//     // accompaniment_synth.triggerAttack("F3");
-//     // await new Promise(r => setTimeout(r, 800));
-//     // accompaniment_synth.triggerRelease(["G2", "B2", "D3", "F3"])
-//     // await new Promise(r => setTimeout(r, 200));
-// }
-// async function cmbass() {
-//     console.log("Insert bass here");
-//     // for (let i = 0; i < 3; i += 1) {
-//     //     bass_synth.triggerAttackRelease("C3", "8N");
-//     //     await new Promise(r => setTimeout(r, 250));
-//     //     bass_synth.triggerAttackRelease("Bb2", "8N");
-//     //     await new Promise(r => setTimeout(r, 250));
-//     //     bass_synth.triggerAttackRelease("Ab2", "8N");
-//     //     await new Promise(r => setTimeout(r, 250));
-//     //     bass_synth.triggerAttackRelease("G2", "8N");
-//     //     await new Promise(r => setTimeout(r, 250));
-//     // }
-//     //
-//     // bass_synth.triggerAttackRelease("C3", "8N");
-//     // await new Promise(r => setTimeout(r, 250));
-//     // bass_synth.triggerAttackRelease("G2", "8N");
-//     // await new Promise(r => setTimeout(r, 250));
-//     // bass_synth.triggerAttackRelease("C3", "16N");
-//     // await new Promise(r => setTimeout(r, 125));
-//     // bass_synth.triggerAttackRelease("D3", "8N");
-//     // await new Promise(r => setTimeout(r, 250));
-//     // bass_synth.triggerAttackRelease("Eb3", "16N");
-//     // await new Promise(r => setTimeout(r, 125));
-//     // bass_synth.triggerAttackRelease("C3", "8N");
-//     // await new Promise(r => setTimeout(r, 250));
-// }
