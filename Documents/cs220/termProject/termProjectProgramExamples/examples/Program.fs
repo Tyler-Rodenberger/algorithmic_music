e37a9ac2cf2ddcfd7d37bdf4f400040cb99ea29b
Tyler Rodenberger <trodenbe@hamilton.edu>
2023-04-10 17:51:29 -0400

    Adding term project program examples final copy

---
+++ Documents/cs220/termProject/termProjectProgramExamples/examples/Program.fs
@@ -0,0 +1,163 @@
+﻿(*|
+Term Project Stage 3: Program Examples
+
+Project: Program Examples
+Author: Tyler Rodenberger
+Date: 4/10/23
+Language: F#
+
+Project Description:
+The following examples show off a few interesting features of F# -- asynchronous
+functions, matching, and unit specification. Asynchronous functions are used in 
+threading, delaying, timing, etc. and async blocks allow for all of these to 
+be done relatively easily. Matching is one of the key features of F#. Its 
+matching system allows not only for really easy comparison but also provides
+a concise way to write conditionals and switch statements. Finally, less integral
+to F# as a whole but an interesting feature nonetheless is unit specification.
+F# has the ability to assign units to values you are working with and 
+automatically figure out the unit of the result of an operation. This is useful
+when performing calculations or if you are particularly concerned with recording
+units.
+
+References:
+https://learn.microsoft.com/en-us/dotnet/fsharp/what-is-fsharp (textbook equivalent)
+https://www.youtube.com/watch?v=c7eNDJN758U (youtube tutorial)
+https://fsharp.org/docs/ (F# Documentation)
+|*)
+
+
+
+
+
+
+// ================== EXAMPLE 1 -- ASYNCHRONOUS FUNCTIONS =========================== \\
+
+
+(*|
+Asynchrony is something I have only ever encountered in JavaScript. It essentially
+allows for threading which is useful for when you want to run more than one piece 
+of code at the same time. In F# this is done by using the async keyword when defining
+a function (it can also exist in its own code block) and either starting the running
+of the code while the rest of the program continues, or by batching several async
+functions together and running them using RunAsynchronously (Which is a bit of a
+misnomer because there is still threading involved)
+|*)
+
+let shortRest = async {
+        "This function waits for one second and returns 1" |> ignore
+        do! Async.Sleep(1000)
+        printfn "I've had a short rest!"
+        return 1
+    }
+
+let longRest = async {
+        "This function waits for three seconds and returns 'monkeys in the bed!'" |> ignore
+        do! Async.Sleep(3000)
+        printfn "I've had a long rest!"
+        return "monkeys in the bed!"
+    }
+
+let allRest = async {
+        "This function batches the two previous asynchronous functions and runs them together" |> ignore
+        // Begin both functions asynchronously
+        let! short = Async.StartChild shortRest
+        let! long = Async.StartChild longRest
+
+        
+        // Wait for the results
+        let! result1 = short
+        let! result2 = long
+
+        return sprintf "%d %s" (result1 + 5) (result2.ToUpper())
+    }
+
+allRest |> Async.RunSynchronously |> printfn "%A"
+
+
+
+
+// ================== EXAMPLE 2 -- RECURSION AND MATCHING =========================== \\
+
+(*|
+The main point of this section is to discuss matching, but there is also some
+recursion involved which is always nice. Matching is a fairly unique feature to
+F# and it allows for easy pattern matching. A similar idea in python or C++ would
+be the switch statement. 
+
+Recursion can be done by calling a function within a function, but also by using the
+rec keyword when defining a function or code block. Tail recursion is only executed
+when the function is set up to support tail recursion (lengthList is not).
+|*)
+
+// NOTE: this is not tail recursive
+let rec lengthList (list : list<'a>) :int =
+    "This function uses recursion and returns the length of an inputed list" |> ignore
+    match list with
+    | []      -> 0
+    | first :: rest -> 1 + lengthList rest
+
+let myList = [1; 2; 3; 4; 5; 6; 7]
+
+lengthList myList |> printfn "%A"
+
+
+let getColor r g b =
+    "This function determines the color with given r, g, and b values" |> ignore
+    match r, g, b with 
+    | 0,    0,      0       -> "Black"
+    | 255,  0,      0       -> "Red"
+    | 0,    255,    0       -> "Green"
+    | 0,    0,      255     -> "Blue"
+    | 255,  255,    0       -> "Yellow"
+    | 255,  0,      255     -> "Magenta"
+    | 0,    255,    255     -> "Cyan"
+    | 255,  255,    255     -> "White"
+    | _                     -> "Other"
+
+let r = 255
+let g = 255
+let b = 0
+
+getColor r g b |> printfn "%A"
+
+
+
+
+// ================== EXAMPLE 3 -- UNITS =========================== \\
+
+(*| 
+This next chunk of code shows off some basic features of units in F#.
+As a way of performing accurate calculations without having to store the
+unit elsewhere or have it assumed, you can assign a unit to a number really
+easily in F#. Beyond what I show here, you can create your own units using
+the following syntax:
+
+[<Measure>] type oodle = m^3     // this makes an oodle = cubic meter
+
+and you can create easy conversions as follows:
+
+let gramsPerKilogram : float<g kg^-1> = 1000.0<g/kg>
+let convertGramsToKilograms (x : float<g>) = x / gramsPerKilogram
+|*)
+
+let rectangleArea l w =
+    "This function calulates the area of a rectangle. " |> ignore
+    l * w
+
+let getVelocity d t =
+    "This function calculates the velocity of an object given distance and time." |> ignore
+    d / t
+
+[<Measure>] type m // initiates the meter (m) unit
+[<Measure>] type s // initiates the second (s) unit
+
+let distance = 100<m> // assigns 100 to distance with a unit meters (m)
+let time = 10<s> // assigns 10 to time with a unit seconds (s)
+
+let velocity = getVelocity distance time // velocity has a unit m/s
+printfn "Velocity is: %A" velocity
+
+let length = 4<m>
+let width = 5<m>
+let area = rectangleArea length width // area has a unit m^2
+printfn "Area is: %A" area
---
+++ Documents/cs220/termProject/termProjectProgramExamples/wordle/Program.fs
@@ -0,0 +1,303 @@
+﻿open System
+open System.IO
+
+(*|
+Term Project Stage 3: Program Examples
+
+Project: Wordle
+Author: Tyler Rodenberger
+Date: 4/10/23
+Language: F#
+
+Project Description:
+This is a command line implementation of the common game, Wordle. The goal is
+to figure out what the target 5-letter word is by guessing other 5-letter words.
+After each guess, the program will reveal if any letters in your guess are 
+correct and in the correct position (green), correct and in the wrong position
+(yellow), or if they are incorrect (default terminal print color). The program
+also provides you with a reference alphabet that records which letters you have 
+found information about in the past. Green = correct letter and position, Yellow =
+correct letter but incorrect position, Red = incorrect letter. The game ends
+when the user correctly guesses the word or runs out of guesses (6).
+
+References:
+https://learn.microsoft.com/en-us/dotnet/fsharp/what-is-fsharp (textbook equivalent)
+https://www.youtube.com/watch?v=c7eNDJN758U (youtube tutorial)
+https://fsharp.org/docs/ (F# Documentation)
+|*)
+
+
+// ==================== FILE I/O =======================
+
+let readWordsFromFile (filename : string) =
+    "Takes no arguments and returns a list of all words in a given file." |> ignore
+    let allWords = File.ReadAllText(filename)
+    allWords.Split '\n'
+
+let chooseSolution() :list<char> = 
+    "returns a random word (as a character list) from the solutionsList" |> ignore
+    let solutionsList = readWordsFromFile "solutions.txt"
+    let rand = Random()
+    let index = rand.Next solutionsList.Length
+    Seq.toList solutionsList.[index]
+
+
+
+
+// ==================== GENERAL TOOLS =======================
+
+let rec insertColor (color :char, alphabetColors :list<char>, alphabetIndex :int) :list<char> =
+    "This function takes a color value (y, g, or u, for example), a list of
+    alphabet colors, and an index to put the color value into the color list.
+    It recursively calls itself to return a new list that is identical to the 
+    original with the color argument replacing the original color at the desired
+    index." |> ignore
+    match alphabetColors with
+    | [] -> []
+    | head :: tail when alphabetIndex = 0 -> color :: tail
+    | head :: tail -> head :: (insertColor(color, tail, (alphabetIndex - 1))) // recursive call
+
+let clearWindow() :unit = 
+    "This function just prints a ton of new lines to 'clear' the current
+    terminal window" |> ignore
+    for i in 0..24 do
+        printf "\n"
+
+let stringifyCharList (charList :list<char>) :string =
+    "This takes a list of characters and returns a string made from
+    the characters" |> ignore
+    let mutable string = ""
+    for letter in charList do
+        string <- string + (Char.ToString letter)
+    string
+
+
+
+
+// ==================== GUESS HANDLING =======================
+
+let validGuess(guess : string) :bool = 
+    let dictionary = readWordsFromFile("fiveLetterWords.txt")
+    if Array.contains guess dictionary then
+        true
+    else
+        false
+
+let readGuess() :list<char> = 
+    "Reads a guess from the user from the command line." |> ignore
+    let guess = Console.ReadLine()
+    if validGuess guess then
+        Seq.toList guess
+    else
+        []
+
+let correctGuess (solution : list<char>, guess : list<char>) :bool =
+    "Returns true if the guess matches the solution and false otherwise" |> ignore
+    if guess = solution then
+        true
+    else
+        false
+
+let getGuessColors (guess : list<char>, solution : list<char>) :list<char> =
+    "getGuessColors takes a guess and a solution in the form of character lists
+    and returns a third list that holds the appropriate character color values
+    based on the guess. Example: guess = round and solution = doors results in
+    ['y'; 'g'; 'u'; 'u'; 'y'] where y = Yellow, g = Green, and u = standard 
+    terminal text color." |> ignore
+    let mutable colorList :list<char> = []
+    let mutable tempSolution :list<char> = solution
+
+    // first pass gets green letters and removes them from consideration
+    for i in 0..(guess.Length - 1) do
+        let guessChar = List.item i guess
+        if List.item i tempSolution = guessChar then
+            tempSolution <- insertColor('_', tempSolution, i) // using insert color to replace a character with an underscore
+            colorList <- 'g'::colorList
+        else
+            colorList <- 'u'::colorList
+    
+    colorList <- List.rev colorList // colorList is built up backwards in pass 1 -- this fixes that
+
+    // second pass gets yellows and removes them from consideration
+    for i in 0..(guess.Length - 1) do
+        let guessChar = List.item i guess
+        if List.contains guessChar tempSolution && (List.item i colorList) <> 'g' then // if the letter is in the solution and has not already been found to be green:
+            let solutionCharIndex = tempSolution |> List.findIndex (fun x -> x = guessChar)
+            let guessCharIndex = i
+            colorList <- insertColor('y', colorList, guessCharIndex)
+            tempSolution <- insertColor('_', tempSolution, solutionCharIndex)
+
+    // printfn "current (reversed) Color List for %s: %s" (stringifyCharList(guess)) (stringifyCharList(colorList))
+    colorList 
+
+
+
+
+// ==================== ALPHABET HANDLING =======================
+
+let getAlphabetColors (guessList :list<list<char>>, solution :list<char>, alphabet :list<char>, alphabetColors :list<char>) :list<char> =
+    "getAlphabetColors determines the colors of the letters of the alphabet
+    to print out after each guess is made. Green meaning the letter has 
+    previously been determined to be in the correct location, yellow meaning
+    the letter is known to be in the work but the location is not known, and
+    red meaning the letter is known to not be in the word." |> ignore
+    let mutable alphColors = alphabetColors
+
+    // first pass -- identify reds
+    for guess in guessList do
+        let guessColors = getGuessColors(guess, solution)
+        // at this point, guess is a character array with each respective color
+        // held at the same index in guessColors
+        for i in 0..(guess.Length - 1) do
+            let guessChar = List.item i guess
+            let guessColor = List.item i guessColors
+            let alphIndex = alphabet |> List.findIndex (fun x -> x = guessChar)
+            // if guessColor is red or yellow, update alphColors as such
+            if guessColor = 'u' then
+                alphColors <- insertColor(guessColor, alphColors, alphIndex)
+
+    // second pass -- identify yellows (overwriting reds as necessary)
+    for guess in guessList do
+        let guessColors = getGuessColors(guess, solution)
+        // at this point, guess is a character array with each respective color
+        // held at the same index in guessColors
+        for i in 0..(guess.Length - 1) do
+            let guessChar = List.item i guess
+            let guessColor = List.item i guessColors
+            let alphIndex = alphabet |> List.findIndex (fun x -> x = guessChar)
+            // if guessColor is red or yellow, update alphColors as such
+            if guessColor = 'y' then
+                alphColors <- insertColor(guessColor, alphColors, alphIndex)
+
+    // third pass -- identify greens (overwriting yellows as necessary)
+    for guess in guessList do
+        let guessColors = getGuessColors(solution, guess)
+        // at this point, guess is a character array with each respective color
+        // held at the same index in guessColors
+        for i in 0..(guess.Length - 1) do
+            let guessChar = List.item i guess
+            let guessColor = List.item i guessColors
+            let alphIndex = alphabet |> List.findIndex (fun x -> x = guessChar) 
+            // if guessColor is green, update the value in alphColors to be 'g'
+            if guessColor = 'g' then
+                alphColors <- insertColor(guessColor, alphColors, alphIndex)
+    alphColors
+
+
+
+// ==================== PRINTING =======================
+
+let printColoredGuess (guess : list<char>, colorList : list<char>) :unit = 
+    "Takes a guess and a color list that holds the appropriate colors of
+    each character in the guess and prints each character out in its
+    associated color." |> ignore
+    for i in 0..(guess.Length - 1) do
+        if List.item i colorList = 'g' then
+            Console.ForegroundColor <- ConsoleColor.Green // changes text color
+            let character = List.item i guess
+            printf "%c" character 
+            Console.ResetColor() // resets color to default
+        elif List.item i colorList = 'y' then
+            Console.ForegroundColor <- ConsoleColor.Yellow
+            let character = List.item i guess
+            printf "%c" character 
+            Console.ResetColor()
+        else
+            let character = List.item i guess
+            printf "%c" character 
+    printf "\n"
+
+let printColoredAlphabet (alphabet : list<char>, alphabetColors : list<char>) :unit = 
+    "Takes an alphabet and its associated color list and prints out a colored
+    alphabet in the same way that printColoredGuess prints a colored guess." |> ignore
+    for i in 0..(alphabet.Length - 1) do
+        let letter = List.item i alphabet
+        let color = List.item i alphabetColors
+        if  color = 'g' then
+            Console.ForegroundColor <- ConsoleColor.Green
+            printf "%c" letter 
+            Console.ResetColor()
+        elif color = 'y' then
+            Console.ForegroundColor <- ConsoleColor.Yellow
+            printf "%c" letter 
+            Console.ResetColor()
+        elif color = 'u' then
+            Console.ForegroundColor <- ConsoleColor.Red
+            printf "%c" letter 
+            Console.ResetColor()
+        else
+            printf "%c" letter 
+    printfn "\n"
+
+let printGuessList (guessList : list<list<char>>, solution : list<char>) :unit = 
+    "This function prints a list of a list of characters as a series of
+    strings using getGuessColors and printGuessColors to ensure each
+    character of each string is the correct color." |> ignore
+    for charlist in guessList do
+        let guessColors = getGuessColors(charlist, solution)
+        printColoredGuess(charlist, guessColors)
+
+let wordle :unit = 
+    "Wordle controls the flow of the wordle game" |> ignore
+    let solution = chooseSolution() // choose solution from solutions list
+
+    let mutable guessList = []
+
+    let alphabet :list<char> = ['a'; 'b'; 'c'; 'd'; 'e'; 'f'; 'g'; 'h'; 'i'; 'j'; 'k'; 'l'; 'm'; 'n'; 'o'; 'p'; 'q'; 'r'; 's'; 't'; 'u'; 'v'; 'w'; 'x'; 'y'; 'z']
+    let mutable alphabetColors :list<char> = ['n'; 'n'; 'n'; 'n'; 'n'; 'n'; 'n'; 'n'; 'n'; 'n'; 'n'; 'n'; 'n'; 'n'; 'n'; 'n'; 'n'; 'n'; 'n'; 'n'; 'n'; 'n'; 'n'; 'n'; 'n'; 'n']
+    let mutable solved = false
+    let mutable guessesRemaining = 6
+
+    clearWindow()
+
+    while solved = false && guessesRemaining <> 0 do
+
+        // get guess from user
+        printfn "Guesses remaining: %d" guessesRemaining
+        printf "Enter your guess: "
+        let mutable guess = readGuess()
+        while guess = [] do
+            printf "That is not a valid 5-letter word. Try again: "
+            guess <- readGuess()
+
+        // add it to the guess list
+        guessList <- guess::guessList
+
+        // update endGame parameters
+        solved <- correctGuess(solution, guess)
+        guessesRemaining <- guessesRemaining - 1
+
+        // do all the round printing and color calculation shenanigans
+        clearWindow()
+
+        guessList <- List.rev guessList
+        printfn "Guesses so far: "
+        printGuessList(guessList, solution)
+        printf "\n"
+        alphabetColors <- getAlphabetColors(guessList, solution, alphabet, alphabetColors)
+        printColoredAlphabet(alphabet, alphabetColors)
+        guessList <- List.rev guessList
+   
+    // game over
+    clearWindow()
+    guessList <- List.rev guessList
+    printGuessList(guessList, solution)
+    guessList <- List.rev guessList
+    printf "\n"
+    if solved = false then
+        printfn "You ran out of guesses. Better luck next time!"
+        let targetWord = stringifyCharList(solution) 
+        printf "The correct word was: " 
+        Console.ForegroundColor <- ConsoleColor.Green
+        printf "%s\n" targetWord
+        Console.ResetColor()
+    else
+        printfn "You solved the puzzle!"
+        let targetWord = stringifyCharList(solution) 
+        printf "The word was: " 
+        Console.ForegroundColor <- ConsoleColor.Green
+        printf "%s\n" targetWord
+        Console.ResetColor()
+
+
+wordle
